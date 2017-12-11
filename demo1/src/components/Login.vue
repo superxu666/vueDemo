2b3a1458a2d1d5748b6ac5a18a05aa810b63b7f0
Eddie <Eddie>
2017-12-11 17:54:26 +0800

    'add'

---
+++ demo1/src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>this is {{mesg}}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        mesg: 'Login'
+      }
+    }
+  }
+</script>
